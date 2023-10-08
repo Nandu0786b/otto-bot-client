02c05fd96520e54984d088916bdd4775fcbc2783
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-10-08 23:01:49 +0530

    sigin page for the user

---
+++ src/components/signin.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
+const Signin = ({ showToast }) => {
+const navigate=useNavigate();
+
+    const handleSignin=async (e)=>{
+      try {
+        localStorage.clear()
+        let iemail=document.getElementById('email').value
+        let ipassword=document.getElementById('password').value
+        e.preventDefault();
+        const response=await fetch("http://localhost:3001/v1/user/login",{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({email:iemail,pass:ipassword})
+    
+    
+        });
+        toast.loading('Loading...');
+        const json=await response.json();
+        console.log(json)
+        toast.dismiss();
+        if(json.Verified===true){
+          showToast(json.message, 'success');
+          localStorage.setItem("token",json.profile.token)
+            
+          navigate("/")
+        }else{
+          showToast(json.message, 'error');
+    
+        }} catch (error) {
+          showToast(error.message, 'error');
+        }
+    }
+
+  return (
+   <>
+<Toaster position='top-center' reverseOrder={false}></Toaster>
+<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to stock alert</h2>
+  </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action="#" method="POST">
+      <div>
+        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div className="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div className="text-sm">
+           
+          </div>
+        </div>
+        <div className="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <button onClick={handleSignin} type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+
+    <p className="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      
+      <Link to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</Link>
+    </p>
+  </div>
+</div>
+
+   
+   </>
+  )
+}
+
+export default Signin
